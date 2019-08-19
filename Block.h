d271353e4b7fcb218386a7b573284687e94e5177
SverreSB <sverre.sb@gmail.com>
2019-08-19 19:48:38 +0200

    added blockchain class defining a blockchain with function declaration to add block and private function declaration to get the last block of the chain, const function so variables wont be manipulated

--- Block.h
+++ Block.h
@@ -2,3 +2,23 @@
 #include <iostream>
 
 using namespace std;
+
+class Block {
+	public:
+		string prevHash;
+
+		Block(uint32_t indexIn, const string &dataIn);
+		
+		string getHash();
+		
+		void mineBlock(uint32_t difficulty);
+
+	private:
+		uint32_t index;
+		int64_t nonce;
+		string data;
+		string hash;
+		time_t time;
+
+		string calculateHash() const;
+};
