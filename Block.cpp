69fd01e9e34d3a7adfb64c96d952f0acfcd89736
SverreSB <sverre.sb@gmail.com>
2019-08-20 22:32:32 +0200

    added function for mining block and claculate hash in Block.cpp

--- Block.cpp
+++ Block.cpp
@@ -1,3 +1,4 @@
+#include <sstream>
 #include "Block.h"
 #include "sha256.h"
 
@@ -12,4 +13,28 @@ Block::Block(uint32_t indexIn, const string &dataIn) {
 
 string Block::getHash() {
 	return hash;
+}
+
+void Block::mineBlock(uint32_t difficulty) {
+	char charArray[difficulty + 1];
+
+	for(uint32_t i = 0; i < difficulty; i++) {
+		charArray[i] = '0';
+	}
+
+	charArray[difficulty] = '\0';
+
+	string str = charArray;
+
+	do {
+		nonce++;
+		hash = calculateHash();
+	} while (hash.substr(0, difficulty) != str);
+}
+
+string Block::calculateHash() const {
+	stringstream ss;
+    ss << index << timestamp << data << nonce << prevHash;
+ 
+    return sha256(ss.str());
 }
